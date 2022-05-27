2943f6ce282a1709580fd2fc1a9cb0106fa4173a
vthelamb <vglamb8@gmail.com>
2022-05-27 17:14:12 -0300

    submit button customised

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,10 +7,13 @@ function Form(){
                 <label>Name: </label>
                 <input type='text' className='input-name' placeholder='Your name...'></input>
 
-                <label>  Email: </label>
+                <label> Email: </label>
                 <input type='text' className='input-email' placeholder='Your e-mail address...'></input>
-                
-                <input type='submit' value='Submit'/>
+
+                <label> Telephone: </label>
+                <input type="tel" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" className='input-phone' placeholder='Your phone number...'></input>
+                <br></br>
+                <input type='submit' className='submit-button' value='Submit'/>
             </form>
         </div>
     )
